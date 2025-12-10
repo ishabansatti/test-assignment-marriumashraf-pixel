216c369c03a696fe10332a38b8d9cc919f8e8463
marriumashraf-pixel <marriumashraf@gmail.com>
2025-12-10 02:52:40 -0800

    Update sum_numbers.py

--- sum_numbers.py
+++ sum_numbers.py
@@ -4,5 +4,13 @@ def sum_numbers(*args):
     If no arguments are passed, returns 0.
     """
     # TODO: implement the logic
-    # total = sum(args)
+    #total = sum(args)
+    return total
+def sum_numbers(*args):
+    """
+    Returns the sum of all input numbers.
+    If no arguments are passed, returns 0.
+    """
+    # TODO: implement the logic
+    total = sum(args)
     return total
